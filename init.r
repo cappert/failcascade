2dc9f2f9eccd07a9b17e97eef8a3bdd382c413ec
Toms Mikoss <toms.mikoss@gmail.com>
2013-08-17 11:46:07 +0300

    Do not re-install forecast each time

--- init.r
+++ init.r
@@ -1,1 +1,3 @@
-install.packages("forecast", repos='http://cran.r-project.org', dependencies = TRUE)
+if(!is.element('forecast', installed.packages()[,1])){
+  install.packages("forecast", repos='http://cran.r-project.org', dependencies = TRUE)
+}
