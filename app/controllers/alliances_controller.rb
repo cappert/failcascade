afa4556dcf1de5ebcc5bade07f680025628cd2a4
Toms Mikoss <toms.mikoss@gmail.com>
2013-08-17 13:22:05 +0300

    Basic info

--- app/controllers/alliances_controller.rb
+++ app/controllers/alliances_controller.rb
@@ -1,4 +1,9 @@
 class AlliancesController < ApplicationController
   def index
+    @alliances = Alliance.all.desc(:current_member_count).limit(25)
+  end
+
+  def show
+    @alliance = Alliance.where(ticker: params[:ticker]).first
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,4 @@
 Failcascade::Application.routes.draw do
   root to: 'alliances#index'
+  get '/:ticker', to: 'alliances#show', as: :alliance
 end
