0eabcc2b3895a90ae645103c961c74bd79e53d89
Toms Mikoss <toms.mikoss@gmail.com>
2013-08-19 23:34:15 +0300

    Extract list

--- app/controllers/alliances_controller.rb
+++ app/controllers/alliances_controller.rb
@@ -2,27 +2,31 @@ class AlliancesController < ApplicationController
   before_filter :set_alliances, except: [:show]
 
   def index
+    @alliances = @alliances.desc(:current_member_count)
+
     if params[:q].to_s.present?
       regex = /.*#{Regexp.escape params[:q].to_s.strip}.*/i
       @alliances = @alliances.or({ name: regex }, { ticker: regex })
     end
 
     respond_to do |format|
-      format.html
+      format.html do
+        if @alliances.size == 1
+          redirect_to alliance_path(id: @alliances.first.ticker)
+        end
+      end
       format.json do
         render json: @alliances.map{ |alliance| { id: alliance.ticker, text: "#{alliance.name} <#{alliance.ticker}>" } }
       end
     end
   end
 
-  def top
+  def top_list
     @alliances = @alliances.desc(:current_member_count)
-    render :index
   end
 
   def growing
-    @alliances = @alliances.desc(:growth_ratio, :current_member_count)
-    render :index
+    @alliances = @alliances.gt(current_member_count: 100).desc(:growth_ratio, :current_member_count)
   end
 
   def collapsing
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Failcascade::Application.routes.draw do
   root to: 'landing#index'
   resources :alliances, only: [:index, :show], constraints: { id: /.*/ } do
-    get :top, on: :collection
+    get :top_list, on: :collection
     get :growing, on: :collection
     get :collapsing, on: :collection
   end
