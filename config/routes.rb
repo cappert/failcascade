a99e7d77cf71e7d6fe586ad3401689da718a9cc5
Toms Mikoss <toms.mikoss@gmail.com>
2013-08-22 00:15:39 +0300

    About page

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Failcascade::Application.routes.draw do
   root to: 'landing#index'
+
+  get '/about', to: 'landing#about', as: :about
+
   resources :alliances, only: [:index, :show], constraints: { id: /.*/ } do
     get :top_list, on: :collection
     get :growing, on: :collection
