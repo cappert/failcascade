bdb1ab02f5bec771c3fdf173c7a2bf16c9db58ea
Toms Mikoss <toms.mikoss@gmail.com>
2013-08-22 23:03:57 +0300

    seed query

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,3 +2,11 @@ paths = Dir[Rails.root.join 'db', 'seed_data', '*.xml'].sort
 paths.each do |path|
   Alliance.update_from_file path, predict: path == paths.last
 end
+
+# select ms.solarSystemId as id, ms.solarSystemName as name, ms.regionid, mr.regionname, ms.constellationid, mc.constellationname
+# from mapSolarSystems ms, mapRegions mr, mapConstellations mc
+# where ms.regionid = mr.regionid
+# and ms.constellationid = mc.constellationid
+# and ms.security < 0
+# and mr.regionname not like '%-%'
+# order by ms.solarsystemid asc
