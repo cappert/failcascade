4db6c56191162b0871a0e730e9dc63467db48b8b
Toms Mikoss <toms.mikoss@gmail.com>
2013-11-01 13:13:19 +0200

    Force dependencies

--- init.r
+++ init.r
@@ -1,3 +1,4 @@
 if(!is.element('forecast', installed.packages()[,1])){
+  install.packages("Rcpp", repos='http://cran.r-project.org', dependencies = TRUE)
   install.packages("forecast", repos='http://cran.r-project.org', dependencies = TRUE)
 }
