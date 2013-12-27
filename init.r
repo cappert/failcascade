f9a83ff7dda6569cbd458ade87e245914948bdbf
Toms Mikoss <toms.mikoss@gmail.com>
2013-12-27 10:26:57 +0200

    even older rcpp

--- init.r
+++ init.r
@@ -1,4 +1,4 @@
 if(!is.element('forecast', installed.packages()[,1])){
-  install.packages("http://cran.r-project.org/src/contrib/Archive/Rcpp/Rcpp_0.9.15.tar.gz", contriburl=NULL, type="source")
+  install.packages("http://cran.r-project.org/src/contrib/Archive/Rcpp/Rcpp_0.9.10.tar.gz", contriburl=NULL, type="source")
   install.packages("forecast", repos='http://cran.r-project.org', dependencies = TRUE)
 }
