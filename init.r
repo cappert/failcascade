e998a02d6e0b74ebb240fc912ad86f330a41b4fb
Toms Mikoss <toms.mikoss@gmail.com>
2013-12-27 12:01:31 +0200

    Try R 3.0.1

--- init.r
+++ init.r
@@ -1,4 +1,1 @@
-if(!is.element('forecast', installed.packages()[,1])){
-  install.packages("http://cran.r-project.org/src/contrib/Archive/Rcpp/Rcpp_0.9.12.tar.gz", contriburl=NULL, type="source")
-  install.packages("forecast", repos='http://cran.r-project.org', dependencies = TRUE)
-}
+install.packages("forecast", repos='http://cran.r-project.org', dependencies = TRUE)
